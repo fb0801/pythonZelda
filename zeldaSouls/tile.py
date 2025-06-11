a120966f80002229d12f36dab850dfd7bfb40c06
fb0801 <fbhatti0801@gmail.com>
2025-06-11 17:58:36 +0100

    level setup 3

--- zeldaSouls/tile.py
+++ zeldaSouls/tile.py
@@ -1,7 +1,7 @@
 import pygame 
 from settings import *
 
-class Title(pygame.sprite.Sprite):
+class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.image.load("../graphics/test/rock.png").convert_alpha()
