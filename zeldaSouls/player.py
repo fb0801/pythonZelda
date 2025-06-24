3f0343af532edf9fd7316204de225e53c5c6ef56
fb0801 <fbhatti0801@gmail.com>
2025-06-24 17:49:50 +0100

    creating the player 1.3

--- zeldaSouls/player.py
+++ zeldaSouls/player.py
@@ -2,13 +2,15 @@ import pygame
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
         self.image = pygame.image.load("../graphics/test/player.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
 
         self.direction = pygame.math.Vector2()
         self.speed = 5
+
+        self.obstacle_sprites = obstacle_sprites
 
     def input(self):
         keys=pygame.key.get_pressed()
@@ -31,8 +33,20 @@ class Player(pygame.sprite.Sprite):
     def move(self, speed):
         if self.direction.magnitude() !=0:
             self.direction = self.direction.normalize()
-            
+
         self.rect.center += self.direction * speed
+
+    def collision(self, direction):
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0: #mve right
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x <0:
+                        self.rect.left = sprite.rect.right
+        
+        if direction == 'vertical':
+            pass
 
     def update(self):
         self.input()
