38d3fbb7d4fcc5b720ba0732a77253ce7530dd68
fb0801 <fbhatti0801@gmail.com>
2025-07-08 17:52:11 +0100

    creating the camera

--- zeldaSouls/level.py
+++ zeldaSouls/level.py
@@ -12,7 +12,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         #sprite grp setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         #sprite setup
@@ -31,5 +31,23 @@ class Level:
 
     def run(self):
         # update and draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw()
         self.visible_spritesx.update()
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __ini__(self):
+
+        #general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+
+
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft + self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
