1cfd7982cc7fa2f2f602bafbee060e19dce130cf
fb0801 <fbhatti0801@gmail.com>
2025-07-09 17:40:46 +0100

    creating the camera 3

--- zeldaSouls/level.py
+++ zeldaSouls/level.py
@@ -43,8 +43,6 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.display_surface = pygame.display.get_surface()
         self.half_width = self.display_surface.get_size()[0] // 2
         self.half_height = self.display_surface.get_size()[1] // 2
-
-
         self.offset = pygame.math.Vector2()
 
     def custom_draw(self, player):
