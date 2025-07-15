6dd0ae7e9747d8fc7bca8bf9bc0d16e8b47c0127
fb0801 <fbhatti0801@gmail.com>
2025-07-15 17:47:03 +0100

    graphcis and missing files

--- zeldaSouls/level.py
+++ zeldaSouls/level.py
@@ -45,6 +45,10 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
+        #creating the floor
+        self.floor_surf = pygame.image.load('../graphics/tilemap/ground.png').convert()
+        self.floor.rect = self.floor_surf.get_rect(topleft =(0,0))
+
     def custom_draw(self, player):
         #getting the offset
         self.offset.x = player.rect.centerx - self.half_width
