f9e064a03961c9fd71a80971cd2da57dec8dbfac
fb0801 <fbhatti0801@gmail.com>
2025-08-06 18:04:34 +0100

    player weapons 2

--- zeldaSouls/code/weapon.py
+++ zeldaSouls/code/weapon.py
@@ -3,5 +3,11 @@ import pygame
 class Weapon(pygame.sprite.Sprite):
     def __init__(self, player, groups):
         super().__init__(groups)
+        direction = player.status.split('_')[0]
+
+        #graphic
         self.image = pygame.Surface((40,40))
-        self.rect = self.image.get_rect(center = player.rect.center)
+        
+        #placement
+        if direction == 'right':
+            self.rect = self.image.get_rect(center = player.rect.center)
