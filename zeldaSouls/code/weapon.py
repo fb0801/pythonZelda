d25fd4d1559d02e7541a7b3dbbc3b0ec565b61ad
fb0801 <fbhatti0801@gmail.com>
2025-08-12 17:34:13 +0100

    player weapons 2.2

--- zeldaSouls/code/weapon.py
+++ zeldaSouls/code/weapon.py
@@ -6,6 +6,7 @@ class Weapon(pygame.sprite.Sprite):
         direction = player.status.split('_')[0]
 
         #graphic
+        full_path = f'../graphics/weapons{player.weapon}/{direction}.png'
         self.image = pygame.Surface((40,40))
         
         #placement
@@ -13,6 +14,7 @@ class Weapon(pygame.sprite.Sprite):
             self.rect = self.image.get_rect(midleft = player.rect.midright + pygame.math.Vector2(0,16))
         elif direction == 'left':
             self.rect = self.image.get_rect(midright = player.rect.midleft + pygame.math.Vector2(0,16))
-
+        elif direction == 'down':
+            self.rect = self.image.get_rect(midtop = player.rect.midbottom + pygame.math.Vector2(-10,0))
         else:
-            self.rect = self.image.get_rect(center = player.rect.center)
+            self.rect = self.image.get_rect(midbottom = player.rect.midtop + pygame.math.Vector2(-10,0))
