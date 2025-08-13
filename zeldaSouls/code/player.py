c1f52659d437e39de114a8d414e9999e606d9343
fb0801 <fbhatti0801@gmail.com>
2025-08-13 17:48:23 +0100

    player weapons 2.5 fix

--- zeldaSouls/code/player.py
+++ zeldaSouls/code/player.py
@@ -86,8 +86,11 @@ class Player(pygame.sprite.Sprite):
                 self.can_switch_weapon = False
                 self.weapon_switch_time = pygame.time.get_ticks()
                 
-                if self.
-                self.weapon_index += 1
+                if self.weapon_index < len(list(weapon_data.keys())) - 1:
+                    self.weapon_index += 1
+                else:
+                    self.weapon_index = 0
+
                 self.weapon = list(weapon_data.keys())[self.weapon_index]
 
 
