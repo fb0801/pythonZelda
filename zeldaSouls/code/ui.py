548907eade751b40de615c7bb2b143916c3b1350
fb0801 <fbhatti0801@gmail.com>
2025-08-19 17:41:55 +0100

    player ui 1.4

--- zeldaSouls/code/ui.py
+++ zeldaSouls/code/ui.py
@@ -12,5 +12,18 @@ class UI:
         self.health_bar_rect = pygame.Rect(10,10,HEALTH_BAR_WIDTH,BAR_HEIGHT)
         self.energy_bar_rect = pygame.Rect(10,34, ENERGY_BAR_WIDTH, BAR_HEIGHT)
 
+    def show_bar(self, current, max_amount, bg_rect, color):
+        # draw bg
+        pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)
+
+        # converting stat to pixel
+        ratio = current / max_amount
+        current_width = bg_rect.width * ratio
+        
+        # draw the bar
+
+
+
     def display(self, player):
-        pass
+        self.show_bar(player.health, player.stats['health'], self.health_bar_rect, HEALTH_COLOR)
+        self.show_bar(player.energy, player.stats['energy'], self.energy_bar_rect, ENERGY_COLOR)
