dd407f184bd557c17d27ef9a4b21e3b3f42f83db
fb0801 <fbhatti0801@gmail.com>
2025-09-16 17:39:24 +0100

    enemies and player interactions 4

--- zeldaSouls/code/enemy.py
+++ zeldaSouls/code/enemy.py
@@ -94,6 +94,12 @@ class Enemy(Entity):
             if current_time - self.attack_time >= self.attack_cooldown:
                 self.can_attack = True
 
+    def get_damage(self, player, attack_type):
+        if attack_type == 'weapon':
+            self.health -= player.get_full_weapon_damage()
+        else:
+            pass
+
     def update(self):
         self.move(self.speed)
         self.animate()
