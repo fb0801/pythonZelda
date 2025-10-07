835455b7c7ddb924e97c352c3890383a90bf344d
fb0801 <fbhatti0801@gmail.com>
2025-10-07 16:49:21 +0100

    particles 1.9

--- zeldaSouls/code/level.py
+++ zeldaSouls/code/level.py
@@ -104,8 +104,9 @@ class Level:
                     for target_sprite in collision_sprites:
                         if target_sprite.sprite_type == 'grass':
                             pos = target_sprite.rect.center
+                            offset = pygame.math.Vector2(0,75)
                             for leaf in range(randint(3,6)):
-                                self.animation_player.create_grass_particles(pos, [self.visible_sprites])
+                                self.animation_player.create_grass_particles(pos - offset, [self.visible_sprites])
                             target_sprite.kill()
                         else:
                             target_sprite.get_damage(self.player, attack_sprite.sprite_type)
@@ -115,6 +116,7 @@ class Level:
             self.player.health -= amount
             self.player.vulnerable = False
             self.player.hurt_time = pygame.time.get_ticks()
+            self.animation_player.create_particles(attack_type, self.player.rect.center, [self.visible_sprites])
      
 
 
