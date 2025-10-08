8af57218d0d96dc0226f2ce11ce68b1537b86cc7
fb0801 <fbhatti0801@gmail.com>
2025-10-08 17:14:35 +0100

    spells 1.2

--- zeldaSouls/code/player.py
+++ zeldaSouls/code/player.py
@@ -209,6 +209,13 @@ class Player(Entity):
         weapon_damage = weapon_data[self.weapon]['damage']
         
         return base_damage + weapon_damage
+    
+    def energy_recovery(self):
+        if self.energy <= self.stats['energy']: 
+            self.energy += 1
+        else:
+            self.energy = self.stats['energy']
+
 
     def update(self):
         self.input()
@@ -216,3 +223,4 @@ class Player(Entity):
         self.get_status()
         self.animate()
         self.move(self.speed)
+        self.energy_recovery()
