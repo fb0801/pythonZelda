f3dae1c63d997c37fbf0bcad8258af29fa26c051
fb0801 <fbhatti0801@gmail.com>
2025-10-14 17:35:40 +0100

    upgrade system 4

--- zeldaSouls/code/upgrade.py
+++ zeldaSouls/code/upgrade.py
@@ -6,6 +6,27 @@ class Upgrade:
         #general setup
         self.display_surface = pygame.display.get_surface()
         self.player = player
+        self.attribute_nr = len(player.stats)
+        self.attribute_names = list(player.stats.keys())
+        self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
+
+        #selection sys
+        self.selection_index = 0
+        self.selection_time = None
+        self.can_move = True
+
+    def input(self):
+        keys= pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            pass
+        elif keys[pygame.K_LEFT]:
+            pass
+        
+        if keys[pygame.K_SPACE]:
+            pass
+
+
 
     def display(self):
         self.display_surface.fill('black')
